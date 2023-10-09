cba6899cb8c1ac6f2f5420acf5e575a041567006
pranaykumar <pranay.kumar.chalamalla@amd.com>
2023-10-09 14:37:56 +0530

    Added copyright message to collect_logs.sh file.
    Signed-off-by: pranaykumar <pranay.kumar.chalamalla@amd.com>

--- src/collect_logs.sh
+++ src/collect_logs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+##
+# Copyright (c) 2023 Advanced Micro Devices, Inc.  All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+##
+
 rm -rf static/tmp
 LOGNAME=log_$(date +"%d_%m_%Y-%H_%M")
 LOGDIR=/tmp/${LOGNAME}
